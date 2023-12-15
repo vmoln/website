10f9051a6537a0b0bd91dfd554cf9d3077299491
vmoln <vgavrishkiv7@gmail.com>
2023-12-15 16:18:23 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,97 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let bodyLockStatus = true;
+    let bodyLockToggle = (delay = 500) => {
+        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+    };
+    let bodyUnlock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
+                }
+                body.style.paddingRight = "0px";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            }
+            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                bodyLockToggle();
+                document.documentElement.classList.toggle("menu-open");
+            }
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.addEventListener("DOMContentLoaded", (() => {
+        document.addEventListener("click", documentActions);
+        function documentActions(e) {
+            const targetEl = e.target;
+            if (window.innerWidth < 479.98) {
+                if (targetEl.classList.contains("menu__icon")) targetEl.closest(".header__menu").classList.toggle("_open");
+                if (!targetEl.closest(".menu__icon")) {
+                    _removeClasses(document.querySelectorAll(".header__menu._open"), "_open");
+                    _removeClasses(document.querySelectorAll(".menu-open"), "menu-open");
+                    _removeClasses(document.querySelectorAll(".lock"), "lock");
+                }
+            }
+        }
+        window.addEventListener("resize", (function() {
+            if (window.innerWidth > 479.98) {
+                _removeClasses(document.querySelectorAll(".header__menu._open"), "_open");
+                _removeClasses(document.querySelectorAll(".menu-open"), "menu-open");
+            }
+        }));
+    }));
+    function _removeClasses(el, class_name) {
+        for (let i = 0; i < el.length; i++) el[i].classList.remove(class_name);
+    }
+    window["FLS"] = true;
+    isWebp();
+    menuInit();
+})();
